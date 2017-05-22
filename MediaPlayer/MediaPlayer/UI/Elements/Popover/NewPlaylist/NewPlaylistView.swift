a7fe9a0aac6bae8eb4f0d2d1249f48e4e4b34b14
chriswebb09 <christopher.webb@flatironschool.com>
2017-05-22 01:22:11 -0400

    update

--- MediaPlayer/MediaPlayer/UI/Elements/Popover/NewPlaylist/NewPlaylistView.swift
+++ MediaPlayer/MediaPlayer/UI/Elements/Popover/NewPlaylist/NewPlaylistView.swift
@@ -1,27 +1,5 @@
 import UIKit
 
-struct DetailViewConstants {
-    static let heightMultiplier: CGFloat = 0.2
-    static let fieldWidth: CGFloat = 0.9
-    static let borderWidth: CGFloat = 2
-    static let largeCornerRadius: CGFloat = 5
-    static let cornerRadius: CGFloat = 2
-    static let shadowOpacity: Float = 0.5
-    static let shadowOffset = CGSize(width: 0, height: 2)
-    static let mainColor: UIColor = UIColor(red:0.13, green:0.21, blue:0.44, alpha:1.0)
-    static let titleFont: UIFont = UIFont(name: "Avenir-Book", size: 18)!
-}
-
-struct PlaylistViewControllerConstants {
-    static let itemSize =  CGSize(width: UIScreen.main.bounds.width, height: 150)
-    static let mainColor = UIColor(red:0.13, green:0.21, blue:0.44, alpha:1.0)
-    static let backgroundColor = UIColor(red:0.97, green:0.97, blue:0.97, alpha:1.0)
-    static let edgeInset = UIEdgeInsets(top:10, left: 0, bottom: 60, right: 0)
-    static let collectionViewEdge = UIEdgeInsets(top:0, left: 0, bottom: 60, right: 0)
-    static let minimumSpace: CGFloat = 20
-    static let collectionItemSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height / 2)
-}
-
 final class NewPlaylistView: UIView {
     
     // Input for playlist name
