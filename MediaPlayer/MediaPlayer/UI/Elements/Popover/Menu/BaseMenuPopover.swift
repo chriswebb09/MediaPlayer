70b2a855aca6d954ca0cba9dd851df5da68af572
chriswebb09 <christopher.webb@flatironschool.com>
2017-05-24 07:22:24 -0400

    added more tests

--- MediaPlayer/MediaPlayer/UI/Elements/Popover/Menu/BaseMenuPopover.swift
+++ MediaPlayer/MediaPlayer/UI/Elements/Popover/Menu/BaseMenuPopover.swift
@@ -15,3 +15,22 @@ class BaseMenuPopoverAlert: UIView {
         viewController.view.sendSubview(toBack: containerView)
     }
 }
+
+
+class BasePopoverMenu: UIView {
+    
+    let containerView: UIView = {
+        let containerView = UIView()
+        containerView.backgroundColor = UIColor.black
+        containerView.layer.opacity = 0.2
+        return containerView
+    }()
+    
+    func showPopView(viewController: UIViewController) {
+        viewController.view.addSubview(containerView)
+    }
+    
+    func hidePopView(viewController: UIViewController){
+        viewController.view.sendSubview(toBack: containerView)
+    }
+}
