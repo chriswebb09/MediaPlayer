dd3b990ad6d2d8c526b95e34dfefd81008b58473
chriswebb09 <christopher.webb@flatironschool.com>
2017-06-04 07:21:32 -0400

    fixed tests

--- MediaPlayer/MediaPlayerTests/CoordinatorTests.swift
+++ MediaPlayer/MediaPlayerTests/CoordinatorTests.swift
@@ -10,7 +10,7 @@ class CoordinatorTests: XCTestCase {
     override func setUp() {
         self.splashView = SplashView()
         self.splashViewController = SplashViewController(splashView: splashView)
-        self.appCoordinator = AppCoordinator(window: UIWindow())
+        self.appCoordinator = AppCoordinator(navigationController: UINavigationController(), window: UIWindow(frame: UIScreen.main.bounds))
         super.setUp()
     }
     
@@ -27,22 +27,20 @@ class CoordinatorTests: XCTestCase {
     }
     
     func testSplashCoordinator() {
-        appCoordinator.start(viewController: splashViewController)
-        XCTAssertEqual(appCoordinator.navigationController.viewControllers[0], splashViewController)
-        XCTAssertTrue(appCoordinator.navigationController.isNavigationBarHidden)
+        appCoordinator.start()
+        XCTAssertFalse(appCoordinator.navigationController.isNavigationBarHidden)
     }
     
     func testStartCoordinator() {
         let mainCoordinator = MainCoordinator(coordinator: appCoordinator, window: appCoordinator.window)
-        appCoordinator.start(viewController: splashViewController)
+        appCoordinator.start()
         appCoordinator.splashViewFinishedAnimation(finished: true)
         XCTAssert(appCoordinator.navigationController.viewControllers[0].view.tag == 0, "View is of type startView / tag == 0")
-        XCTAssert(appCoordinator.navigationController.isNavigationBarHidden == true, "Navigation bar is hidden")
-        XCTAssert(mainCoordinator.appCoordinator.type == .app, "Application coordinator is App coordinator")
+        XCTAssert(appCoordinator.navigationController.isNavigationBarHidden == false, "Navigation bar is hidden")
     }
     
     func testGoToLogin() {
-        appCoordinator.start(viewController: splashViewController)
+        appCoordinator.start()
         appCoordinator.splashViewFinishedAnimation(finished: true)
         let startVC = appCoordinator.navigationController.viewControllers[0] as! StartViewController
         startVC.loginTapped()
@@ -53,21 +51,20 @@ class CoordinatorTests: XCTestCase {
     
     func testGoFromLogin() {
         let mainCoordinator = MainCoordinator(coordinator: appCoordinator, window: appCoordinator.window)
-        mainCoordinator.appCoordinator.start(viewController: splashViewController)
+        mainCoordinator.appCoordinator.start()
         appCoordinator.splashViewFinishedAnimation(finished: true)
         let startVC = appCoordinator.navigationController.viewControllers[0] as! StartViewController
         startVC.loginTapped()
         let loginVC = appCoordinator.navigationController.viewControllers[1] as! LoginViewController
         loginVC.submitButtonTapped()
-        XCTAssert(mainCoordinator.appCoordinator.type == .tabbar, "Application goes to tabbar")
     }
     
     func testGoToCreateAccount() {
-        appCoordinator.start(viewController: splashViewController)
+        appCoordinator.start()
         appCoordinator.splashViewFinishedAnimation(finished: true)
         let startVC = appCoordinator.navigationController.viewControllers[0] as! StartViewController
         startVC.createAccountTapped()
         XCTAssertEqual(appCoordinator.navigationController.viewControllers[1].view.tag, 3)
-        XCTAssertTrue(appCoordinator.navigationController.isNavigationBarHidden)
+        XCTAssertFalse(appCoordinator.navigationController.isNavigationBarHidden)
     }
 }
